5c5276ee5a8d00883b9a0868fd128c371216b7e3
Rajesh Khumanthem <rajesh.khumanthem.gmail.com>
2017-06-18 18:42:08 -0400

    more refactoring

--- message.cpp
+++ message.cpp
@@ -10,153 +10,115 @@
 
 
 Message::Message(MessageType type)
-    :__messageType(type), __timeToLive(10)
+    :__messageType(type)
 {
-    switch (type)
-    {
-        case MessageType::LOGON:
-        {
-            __typeString = "LOGON";
-            break;
-        }
-        case MessageType::ACK:
-        {
-            __typeString = "ACK";
-            break;
-        }
-        case MessageType::DOWNSTREAM:
-        {
-            __typeString = "DOWNSTREAM";
-           break;
-        }
-        default:
-        {
-            std::stringstream err;
-            err << "Unknow Message type[" << (int)type << "]";
-            THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
-        }
-    }
+
 }
 
-
-QJsonDocument Message::toJson()
+Message::~Message()
 {
-    switch(__messageType)
-    {
-        case MessageType::ACK:
-        {
-            return createAck();
-            break;
-        }
-        case MessageType::DOWNSTREAM:
-        {
-            return createDownstream();
-            break;
-        }
-        default:
-        {
-            std::stringstream err;
-            err << "Unknown message type<" << (int)__messageType << ">";
-            THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
-        }
-    }
+
 }
 
-QJsonDocument Message::createAck()
+AckMessage::AckMessage()
+    :Message(MessageType::ACK)
 {
-    QJsonDocument ackmsg;
-    QJsonObject root;
-    root["to"] = __to.c_str();
-    root["message_id"] = __messageId.c_str();
-    root["message_type"] = __typeString.c_str();
-    ackmsg.setObject(root);
-    return ackmsg;
-}
-QJsonDocument Message::createDownstream()
-{
-    QJsonDocument msg;
-    QJsonObject root;
-    root[msgfieldnames::TO] = __to.c_str();
-    root[msgfieldnames::MESSAGE_TYPE] = __typeString.c_str();
-    root[msgfieldnames::MESSAGE_ID] = __messageId.c_str();
-    root[msgfieldnames::TIME_TO_LIVE] = __timeToLive;
-
-    //TODO later
-    if (!__collapseKey.empty())
-        root["collapse_key"]= __collapseKey.c_str();
-    if (!__priority.empty())
-        root["priority"] = __priority.c_str();
-
-    if (!__contentAvailable.isNull())
-        root["content_available"] = __contentAvailable.toBool();
-    if (!__mutableContent.isNull())
-        root["mutable_content"] = __mutableContent.toBool();
-    if (!__deliveryRecieptRequested.isNull())
-        root["deliver_reciept_requested"] = __deliveryRecieptRequested.toBool();
-    if (!__dryRun.isNull())
-        root["dry_run"] = __dryRun.toBool();
-
-    root[msgfieldnames::FCM_DATA] = __data;
-
-    msg.setObject(root);
-    return msg;
 }
 
-void Message::validate()const
+AckMessage::~AckMessage()
 {
-    switch(__messageType)
-    {
-        case MessageType::ACK:
-        {
-            validateAck();
-            break;
-        }
-        case MessageType::DOWNSTREAM:
-        {
-            validateDownstream();
-            break;
-        }
-        default:
-        {
-            std::stringstream err;
-            err << "Unknown message type<" << (int)__messageType << ">";
-            THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
-        }
-    }
+
 }
 
-void Message::validateAck() const
+void AckMessage::validateAck() const
 {
-    if (__to.empty())
-    {
-        std::stringstream err;
-        err << "Invalid ACK message. 'to' field cannot be empty.";
-        THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
-    }
-    if (__messageId.empty())
+    if (getMessageId().empty())
     {
         std::stringstream err;
         err << "Invalid ACK message. 'message_id' field cannot be empty.";
         THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
     }
-    if (__typeString != "ACK")
-    {
-        std::stringstream err;
-        err << "Invalid ACK message. 'message type' field is not set tp 'ack'.";
-        THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
-    }
 }
 
-void Message::validateDownstream() const
+QJsonDocument AckMessage::toJson()
 {
-  std::cout << "Validate downstream mesage" << std::endl;
+    validateAck();
+
+    QJsonDocument ackmsg;
+    QJsonObject root;
+    root[msgfieldnames::MESSAGE_ID] = getMessageId().c_str();
+    root[msgfieldnames::MESSAGE_TYPE] = "ACK";
+    ackmsg.setObject(root);
+    return ackmsg;
+}
+
+
+/*!
+ * \brief DataMessage::DataMessage
+ */
+DataMessage::DataMessage()
+    : Message(MessageType::DOWNSTREAM),
+     __timeToLive(10)
+{
+}
+
+DataMessage::~DataMessage()
+{
+
+}
+
+QJsonDocument DataMessage::toJson()
+{
+    validate();
+
+    QJsonDocument msg;
+    QJsonObject root;
+
+    // pack gimmm header
+    root[msgfieldnames::MESSAGE_TYPE] = "DOWNSTREAM";
+    root[msgfieldnames::MESSAGE_ID] = getMessageId().c_str();
+    root[msgfieldnames::GROUP_ID] = getGroupId().c_str();
+
+    // pack fcm data
+    QJsonObject fcmdata;
+    fcmdata[fcmfieldnames::TO] = __to.c_str();
+    fcmdata[fcmfieldnames::MESSAGE_ID] = getMessageId().c_str();
+    fcmdata[fcmfieldnames::TIME_TO_LIVE] = __timeToLive;
+
+    if (!__collapseKey.empty())
+        fcmdata[fcmfieldnames::COLLAPSE_KEY]= __collapseKey.c_str();
+    if (!__priority.empty())
+        fcmdata[fcmfieldnames::PRIORITY] = __priority.c_str();
+    if (!__contentAvailable.isNull())
+        fcmdata[fcmfieldnames::CONTENT_AVAILABLE] = __contentAvailable.toBool();
+    if (!__mutableContent.isNull())
+        fcmdata[fcmfieldnames::MUTABLE_CONTENT] = __mutableContent.toBool();
+    if (!__deliveryRecieptRequested.isNull())
+        fcmdata[fcmfieldnames::DELIVER_RECIEPT_REQUESTED] = __deliveryRecieptRequested.toBool();
+    if (!__dryRun.isNull())
+        fcmdata[fcmfieldnames::DRY_RUN] = __dryRun.toBool();
+    if (!__data.empty())
+        fcmdata[fcmfieldnames::DATA] = __data;
+
+
+    root[msgfieldnames::FCM_DATA] = fcmdata;
+
+    msg.setObject(root);
+    return msg;
+}
+
+
+void DataMessage::validate()const
+{
+    std::cout << "Validate downstream mesage" << std::endl;
+    // validate gimmm header
     if (__to.empty() && __condition.empty())
     {
         std::stringstream err;
         err << "Invalid DOWNSTREAM message. Both 'to' && 'condition' field cannot be empty.";
         THROW_INVALID_ARGUMENT_EXCEPTION(err.str());
     }
-    if (__messageId.empty() == true)
+    if (getMessageId().empty() == true)
     {
         std::stringstream err;
         err << "Invalid DOWNSTREAM message. 'message_id' field cannot be empty.";
--- message.h
+++ message.h
@@ -12,22 +12,56 @@
 #define MIN_TTL                         0
 
 typedef std::uint64_t MessageId_t;
+typedef std::string   GroupId_t;
 
-/*!
- * field names in the root JSON message inside a xmpp stanza
- */
+
 namespace msgfieldnames
 {
-  static const char* const STATUS           = "status";
-  static const char* const TO               = "to";
   static const char* const MESSAGE_TYPE     = "message_type";
   static const char* const MESSAGE_ID       = "message_id";
   static const char* const GROUP_ID         = "group_id";
   static const char* const SESSION_ID       = "session_id";
+  static const char* const STATUS           = "status";
   static const char* const ERROR            = "error";
   static const char* const ERROR_DESC       = "error_description";
-  static const char* const FCM_DATA         = "fcm_data";
   static const char* const TIME_TO_LIVE     = "time_to_live";
+  static const char* const FCM_DATA         = "fcm_data";
+}
+
+
+
+/*!
+ * field names in the root JSON message inside a xmpp stanza
+ */
+namespace fcmfieldnames
+{
+  // target
+  static const char* const TO                           = "to";
+  static const char* const CONDITION                    = "condition";
+  // options
+  static const char* const MESSAGE_ID                   = "message_id";
+  static const char* const COLLAPSE_KEY                 = "collapse_key";
+  static const char* const PRIORITY                     = "priority";
+  static const char* const CONTENT_AVAILABLE            = "content_available";
+  static const char* const MUTABLE_CONTENT              = "mutable_content";
+  static const char* const TIME_TO_LIVE                 = "time_to_live";
+  static const char* const DELIVER_RECIEPT_REQUESTED    = "delivery_receipt_requested";
+  static const char* const DRY_RUN                      = "dry_run";
+  // payload
+  static const char* const DATA                         = "data";
+  static const char* const NOTIFICATION                 = "notification";
+  // TODO add notification fields
+
+  // downstream response
+  static const char* const FROM                         = "from"; // who sent; token
+  static const char* const MESSAGE_TYPE                 = "message_type"; // ack/nack
+  static const char* const REGISTRATION_ID              = "registration_id";
+  static const char* const ERROR                        = "error";
+  static const char* const ERROR_DESC                   = "error_description";
+
+  // upstream message
+  static const char* const CATEGORY                     = "category";
+  //static const char* const CONTROL_TYPE               = "control_type";
 }
 
 
@@ -44,12 +78,42 @@ enum class MessageType: char
 };
 
 
-enum class MessageContentType: int
+enum class PayloadType: int
 {
-    UNKNOWN         = 0,
-    DATA            = 1, // Handled by client app. 4KB limit.
-    NOTIFICATION    = 2  // Display message. 2KB limit.
+    UNKNOWN = 0,
+    DATA = 1,
+    NOTIFICATION = 2
+};
 
+class Message
+{
+        MessageType         __messageType;
+        // start GIMMM header
+        GroupId_t           __groupId;
+        std::string         __messageId;
+        // End GIMMM header
+    public:
+        virtual QJsonDocument toJson() = 0;
+        Message(MessageType type);
+        virtual ~Message();
+
+        MessageType             getMessageType() const { return __messageType;}
+        const std::string&      getMessageId() const { return __messageId;}
+        const GroupId_t&        getGroupId() const { return __groupId;}
+
+        void setMessageType(MessageType type) { __messageType = type;}
+        void setMessageId(const std::string& message_id) { __messageId = message_id;}
+        void setGroupId(const std::string& group_id) { __groupId = group_id;}
+};
+
+class AckMessage: public Message
+{
+    public:
+       AckMessage();
+       virtual ~AckMessage();
+       virtual QJsonDocument toJson();
+    private:
+       void validateAck()const;
 };
 
 
@@ -59,17 +123,14 @@ enum class MessageContentType: int
  *        Consists of 3 parts; Target, Options & Data.
  *        See https://firebase.google.com/docs/cloud-messaging/xmpp-server-ref for details.
  */
-class Message
+class DataMessage: public Message
 {
-    MessageType         __messageType;
-    MessageContentType  __messageContentType;
-    std::string         __typeString;
     // Target
     std::string         __to;
+            // or
     std::string         __condition;
 
     //Options
-    std::string         __messageId;
     std::string         __collapseKey;
     std::string         __priority;
     QVariant            __contentAvailable;
@@ -82,13 +143,12 @@ class Message
     QJsonObject         __data;
 
 public:
-    Message(MessageType type = MessageType::UNKNOWN);
-    ~Message() {}
-    QJsonDocument toJson(); // throw std::exception
+    DataMessage();
+    virtual ~DataMessage();
+    virtual QJsonDocument toJson(); // throw std::exception
     void setTo(const std::string& to) { __to = to;}
     void setTo(const char* to) { __to = to;}
     void setCondition(const std::string& condition){ __condition = condition;}
-    void setMessageId(const std::string& message_id) { __messageId = message_id;}
     void setCollapseKey( const std::string& collapse_key) { __collapseKey = collapse_key;}
     void setpriority(const std::string& priority) { __priority = priority;}
     void setContentAvailable(bool content_available) { __contentAvailable = content_available;}
@@ -103,11 +163,8 @@ public:
      */
     void validate()const;
 
-    MessageType getMessageType() const { return __messageType;}
-    MessageContentType getMessageContentType() { return __messageContentType;}
     const std::string& getTo()const { return __to;}
     const std::string& getCondition() const { return __condition;}
-    const std::string& getMessageId() const { return __messageId;}
 
     // OPTIONAL FIELDS
     /*!
@@ -154,8 +211,6 @@ public:
      */
     QVariant           getDryRun()const { return __dryRun;}
 private:
-    QJsonDocument createAck();
-    QJsonDocument createDownstream();
     void validateAck()const;
     void validateDownstream()const;
 };
